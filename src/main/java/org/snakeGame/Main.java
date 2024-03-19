f7fa9d3a7c27eecb65fe3da40ee96fe9fa19752c
aman@browserstack <aman.s@browserstack.com>
2024-03-19 19:31:40 +0530

    initial setup

---
+++ src/main/java/org/snakeGame/Main.java
@@ -0,0 +1,14 @@
+package org.snakeGame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        /*
+        * Board:
+        *  -> cells
+        *
+        *
+        *
+        * */
+    }
+}
